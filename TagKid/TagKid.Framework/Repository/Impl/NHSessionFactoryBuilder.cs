8c9cf6daf6e2e2be537c24a158ecd92b8e539948
Mehmet Ataş <mehmetatasm@gmail.com>
2015-04-24 10:06:51 +0300

    m

---
+++ TagKid/TagKid.Framework/Repository/Impl/NHSessionFactoryBuilder.cs
@@ -0,0 +1,27 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace TagKid.Framework.Repository.Impl
+{
+    public static class NHSessionFactoryBuilder
+    {
+        public static ISessionFactory Build<T>(string connStrName)
+        {
+            return Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(c => c.FromConnectionStringWithKey(connStrName)))
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<T>())
+                .ExposeConfiguration(CreateSchema)
+                .BuildSessionFactory();
+        }
+
+        private static void CreateSchema(Configuration config)
+        {
+            SchemaMetadataUpdater.QuoteTableAndColumns(config);
+            var update = new SchemaUpdate(config);
+            update.Execute(false, true);
+        }
+    }
+}
