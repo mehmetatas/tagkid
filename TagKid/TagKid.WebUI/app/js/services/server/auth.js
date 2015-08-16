8dec389173148a33c5152c74ea5ca3dee1583070
Mehmet Ataş <mehmetatasm@gmail.com>
2015-08-16 20:20:09 +0300

    m

---
+++ TagKid/TagKid.WebUI/app/js/services/server/auth.js
@@ -0,0 +1,9 @@
+﻿app.service("auth", ["tagkid",
+    function (tagkid) {
+        "use strict";
+
+        this.register = function(reuqest, success, error, complete) {
+            tagkid.post("auth", "register", reuqest, success, error, complete);
+        };
+    }
+]);
