751b0d801e5a657bc0bc05fe65d6261e219b381d
Mehmet Ataş <mehmetatasm@gmail.com>
2015-11-15 20:43:02 +0200

    owin spa fix

--- TagKid/TagKid.Framework/Hosting/Owin/OwinSpaMiddleware.cs
+++ TagKid/TagKid.Framework/Hosting/Owin/OwinSpaMiddleware.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Owin;
 
@@ -5,9 +7,13 @@ namespace TagKid.Framework.Hosting.Owin
 {
     public class OwinSpaMiddleware : OwinMiddleware
     {
-        public OwinSpaMiddleware(OwinMiddleware next)
+        private readonly string _defaultFilePath;
+        private string _indexHtml;
+
+        public OwinSpaMiddleware(OwinMiddleware next, string defaultFilePath)
             : base(next)
         {
+            _defaultFilePath = defaultFilePath;
         }
 
         public override async Task Invoke(IOwinContext context)
@@ -16,7 +22,20 @@ namespace TagKid.Framework.Hosting.Owin
 
             if (context.Response.StatusCode == 404)
             {
-                context.Response.Redirect("/");
+                if (_indexHtml == null)
+                {
+                    lock (this)
+                    {
+                        if (_indexHtml == null)
+                        {
+                            var path = Path.Combine(Environment.CurrentDirectory, _defaultFilePath);
+                            _indexHtml = File.ReadAllText(path);
+                        }
+                    }
+                }
+
+                context.Response.StatusCode = 200;
+                context.Response.Write(_indexHtml);
             }
         }
     }
